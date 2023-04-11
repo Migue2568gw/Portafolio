5487a1e1c3f4f2df78f173ef1e09a2933381bccf
Migue2568gw <migue2000gw@gmail.com>
2023-04-11 14:07:25 -0500

    Ajuste adicionando funcion javascript

--- Contenido/js/portafolio.js
+++ Contenido/js/portafolio.js
@@ -1,9 +1,10 @@
 const $app = document.getElementById('appFotos');
 
-function generateAvatar(id) {
-    const src = `Contenido/img/portfolio/COMPOSICIONES-${id}.jpg`;
-    return `
-      <div class="col-lg-4 col-md-6 portfolio-item filter-comp">
+ function generateAvatar(id, categoria , filCate) {
+  const src = `Contenido/img/portfolio/${categoria}-${id}.jpg`;
+
+  return `
+      <div class="col-lg-4 col-md-6 portfolio-item ${filCate}">
         <div class="portfolio-wrap">
           <img src="${src}" class="img-fluid" alt="">
           <div class="portfolio-info">
@@ -17,14 +18,48 @@ function generateAvatar(id) {
 }
 
 async function loadImages() {
-    const images = 10;
+  let categorias = ['COMPOSICIONES', 'DOCUMENTAL', 'BODA_BAUTIZOS', 'BANDAS', 'PRODUCTO'];
 
-    for (let i = 1; i <= images; i++) {
-        $app.innerHTML += generateAvatar(i);
+  for (let i = 0; i < categorias.length; i++) {
+    let filtro = '';
+    let cantidadImg = 0;
+    switch (categorias[i]) {
+      case 'COMPOSICIONES':
+        filtro = 'filter-comp';
+        cantidadImg = 10;
+        break;
+      case 'DOCUMENTAL':
+        filtro = 'filter-doc';
+        cantidadImg = 15;
+        break;
+      case 'BODA_BAUTIZOS':
+        filtro = 'filter-BdBa';
+        cantidadImg = 5;
+        break;
+      case 'BANDAS':
+        filtro = 'filter-BnMet';
+        cantidadImg = 15;
+        break;
+      case 'PRODUCTO':
+        filtro = 'filter-pro';
+        cantidadImg = 6;
+        break;
+      default:
+        filtro = 'filter-comp';
+        break;
     }
-    const gallery = GLightbox({
-        selector: '.portfolio-lightbox'
-    });
+    for (let j = 1; j <= cantidadImg ; j++) {
+      try {
+        const avatar = generateAvatar(j,categorias[i],filtro);    
+          $app.innerHTML += avatar;
+      } catch (error) {
+        console.log(`No se pudo cargar la imagen ${categorias[i]}-${j}.jpg`);
+      }
+    }
+  }
+  const gallery = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
 }
 
 document.addEventListener('DOMContentLoaded', loadImages);
